32b42879bb887bd41141f3f9e8b692a5a18c8128
Julie T <joules2319@gmail.com>
2020-04-11 03:03:35 -0600

    created employee object

---
+++ src/utils/employees.js
@@ -0,0 +1,52 @@
+export var employees = {
+  employees: [
+    {
+      firstName: "Julie",
+      lastName: "Theriault",
+      position: "Team Lead",
+      location: "Corporate"
+    },
+    {
+      firstName: "Elsa",
+      lastName: "Moving",
+      position: "Queen",
+      location: "Arandell"
+    },
+    {
+      firstName: "Valette",
+      lastName: "Renoux",
+      position: "Spy",
+      location: "Luthadel"
+    },
+    {
+      firstName: "Elend",
+      lastName: "Venture",
+      position: "Philosopher",
+      location: "Luthadel"
+    },
+    {
+      firstName: "Jo",
+      lastName: "March",
+      position: "Writer",
+      location: "Concord"
+    },
+    {
+      firstName: "Anne",
+      lastName: "Shirley",
+      position: "Writer",
+      location: "Prince Edward Island"
+    },
+    {
+      firstName: "Julie",
+      lastName: "Boxer",
+      position: "Baby",
+      location: "San Francisco"
+    },
+    {
+      firstName: "Gilbert",
+      lastName: "Blythe",
+      position: "Doctor",
+      location: "Prince Edward Island"
+    }
+  ]
+};
