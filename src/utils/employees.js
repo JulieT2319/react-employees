612b7b42738892bb4c1ba1e8c8e8062d14b94838
Julie T <joules2319@gmail.com>
2020-04-11 16:48:54 -0600

    made employee list an array of objects on it's own

--- src/utils/employees.js
+++ src/utils/employees.js
@@ -1,52 +1,50 @@
-export var employees = {
-  employees: [
-    {
-      firstName: "Julie",
-      lastName: "Theriault",
-      position: "Team Lead",
-      location: "Corporate"
-    },
-    {
-      firstName: "Elsa",
-      lastName: "Moving",
-      position: "Queen",
-      location: "Arandell"
-    },
-    {
-      firstName: "Valette",
-      lastName: "Renoux",
-      position: "Spy",
-      location: "Luthadel"
-    },
-    {
-      firstName: "Elend",
-      lastName: "Venture",
-      position: "Philosopher",
-      location: "Luthadel"
-    },
-    {
-      firstName: "Jo",
-      lastName: "March",
-      position: "Writer",
-      location: "Concord"
-    },
-    {
-      firstName: "Anne",
-      lastName: "Shirley",
-      position: "Writer",
-      location: "Prince Edward Island"
-    },
-    {
-      firstName: "Julie",
-      lastName: "Boxer",
-      position: "Baby",
-      location: "San Francisco"
-    },
-    {
-      firstName: "Gilbert",
-      lastName: "Blythe",
-      position: "Doctor",
-      location: "Prince Edward Island"
-    }
-  ]
-};
+export var employees = [
+  {
+    firstName: "Julie",
+    lastName: "Theriault",
+    position: "Team Lead",
+    location: "Corporate"
+  },
+  {
+    firstName: "Elsa",
+    lastName: "Oldenburg",
+    position: "Queen",
+    location: "Arandell"
+  },
+  {
+    firstName: "Valette",
+    lastName: "Renoux",
+    position: "Spy",
+    location: "Luthadel"
+  },
+  {
+    firstName: "Elend",
+    lastName: "Venture",
+    position: "Philosopher",
+    location: "Luthadel"
+  },
+  {
+    firstName: "Jo",
+    lastName: "March",
+    position: "Writer",
+    location: "Concord"
+  },
+  {
+    firstName: "Anne",
+    lastName: "Shirley",
+    position: "Writer",
+    location: "Prince Edward Island"
+  },
+  {
+    firstName: "Julie",
+    lastName: "Boxer",
+    position: "Baby",
+    location: "San Francisco"
+  },
+  {
+    firstName: "Gilbert",
+    lastName: "Blythe",
+    position: "Doctor",
+    location: "Prince Edward Island"
+  }
+];
