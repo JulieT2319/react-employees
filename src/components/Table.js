5d9d7a36d49eee9f4a31a129a1952b1e24020dc3
Julie T <joules2319@gmail.com>
2020-04-11 20:14:29 -0600

    Added raw data to table display

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,19 +1,32 @@
 import React from 'react'
-
+import { employees } from '../utils/employees'
 function Table() {
   return (
-    <div>
-      <table>
-        <tr>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Position</th>
-          <th>Location</th>
-        </tr>
-      </table>
-    </div>
+
+
+    <table>
+      <tr>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Position</th>
+        <th>Location</th>
+      </tr>
+      {
+        employees.map(person => (
+          <tr>
+            <td>{person.firstName}</td>
+            <td>{person.lastName}</td>
+            <td>{person.position}</td>
+            <td>{person.location}</td>
+          </tr>
+        ))
+      }
+    </table>
+
   )
+
 }
+
 
 
 
