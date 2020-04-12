83d0115c59befd2d723b37a6e9e569c06ad4015a
Julie T <joules2319@gmail.com>
2020-04-12 17:18:12 -0600

    corrected table related display issues

--- src/components/Table.js
+++ src/components/Table.js
@@ -5,22 +5,28 @@ function Table(props) {
 
 
     <table>
-      <tr>
-        <th><button>First Name</button></th>
-        <th><button>Last Name</button></th>
-        <th><button>Position</button></th>
-        <th><button>Location</button></th>
-      </tr>
-      {
-        employees.map(person => (
-          <tr>
-            <td>{person.firstName}</td>
-            <td>{person.lastName}</td>
-            <td>{person.position}</td>
-            <td>{person.location}</td>
-          </tr>
-        ))
-      }
+      <thead>
+
+        <tr>
+          <th><button>First Name</button></th>
+          <th><button>Last Name</button></th>
+          <th><button>Position</button></th>
+          <th><button>Location</button></th>
+        </tr>
+      </thead>
+      <tbody>
+
+        {
+          employees.map(person => (
+            <tr key={person.empid}>
+              <td>{person.firstName}</td>
+              <td>{person.lastName}</td>
+              <td>{person.position}</td>
+              <td>{person.location}</td>
+            </tr>
+          ))
+        }
+      </tbody>
     </table>
 
   )
