51d17d7a686202a716e2c38cf21b6193b1fe2ca4
Julie T <joules2319@gmail.com>
2020-04-12 17:23:57 -0600

    Changed Table to class component

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,40 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { employees } from '../utils/employees'
-function Table(props) {
-  return (
+
+export default class Table extends Component {
+
+  render() {
+    return (
 
 
-    <table>
-      <thead>
+      <table>
+        <thead>
 
-        <tr>
-          <th><button>First Name</button></th>
-          <th><button>Last Name</button></th>
-          <th><button>Position</button></th>
-          <th><button>Location</button></th>
-        </tr>
-      </thead>
-      <tbody>
+          <tr>
+            <th><button>First Name</button></th>
+            <th><button>Last Name</button></th>
+            <th><button>Position</button></th>
+            <th><button>Location</button></th>
+          </tr>
+        </thead>
+        <tbody>
 
-        {
-          employees.map(person => (
-            <tr key={person.empid}>
-              <td>{person.firstName}</td>
-              <td>{person.lastName}</td>
-              <td>{person.position}</td>
-              <td>{person.location}</td>
-            </tr>
-          ))
-        }
-      </tbody>
-    </table>
+          {
+            employees.map(person => (
+              <tr key={person.empid}>
+                <td>{person.firstName}</td>
+                <td>{person.lastName}</td>
+                <td>{person.position}</td>
+                <td>{person.location}</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
 
-  )
+    )
+  }
+
 
 }
 
-
-
-
-export default Table
 
