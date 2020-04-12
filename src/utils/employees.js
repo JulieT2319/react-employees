4ae376d7548407b1bb26fb17925fd488b2b4559c
Julie T <joules2319@gmail.com>
2020-04-12 17:17:55 -0600

    added id to employee list

--- src/utils/employees.js
+++ src/utils/employees.js
@@ -1,47 +1,55 @@
 export var employees = [
   {
+    empid: 1,
     firstName: "Julie",
     lastName: "Theriault",
     position: "Team Lead",
     location: "Corporate"
   },
   {
+    empid: 2,
     firstName: "Elsa",
     lastName: "Oldenburg",
     position: "Queen",
     location: "Arandell"
   },
   {
+    empid: 3,
     firstName: "Valette",
     lastName: "Renoux",
     position: "Spy",
     location: "Luthadel"
   },
   {
+    empid: 4,
     firstName: "Elend",
     lastName: "Venture",
     position: "Philosopher",
     location: "Luthadel"
   },
   {
+    empid: 5,
     firstName: "Jo",
     lastName: "March",
     position: "Writer",
     location: "Concord"
   },
   {
+    empid: 6,
     firstName: "Anne",
     lastName: "Shirley",
     position: "Writer",
     location: "Prince Edward Island"
   },
   {
+    empid: 7,
     firstName: "Julie",
     lastName: "Boxer",
     position: "Baby",
     location: "San Francisco"
   },
   {
+    empid: 8,
     firstName: "Gilbert",
     lastName: "Blythe",
     position: "Doctor",
