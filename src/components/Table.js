952ec390dbc28865823fc1d4469c62633af6b015
Julie T <joules2319@gmail.com>
2020-04-11 19:51:33 -0600

    changed from default to displaying table.

---
+++ src/components/Table.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Table() {
+  return (
+    <div>
+      <table>
+        <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Position</th>
+          <th>Location</th>
+        </tr>
+      </table>
+    </div>
+  )
+}
+
+
+
+export default Table
+
