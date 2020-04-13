0b240cef6e8d3a6ea4b596f1b7924bec1d19870a
Julie T <joules2319@gmail.com>
2020-04-13 01:43:59 -0600

    added state changes for button clicks

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,6 +2,58 @@ import React, { Component } from 'react'
 import { employees } from '../utils/employees'
 
 export default class Table extends Component {
+  state = {
+    employeeSort: employees,
+    sortCol: "",
+    order: true
+  }
+
+  // sortEmployees = () => {
+
+  //   let list = this.state.employeeSort
+  //   function compareAsc(a, b) {
+  //     if (a.firstName < b.firstName) {
+  //       return -1;
+  //     }
+  //     if (a.firstName > b.firstName) {
+  //       return 1;
+  //     }
+
+  //     return 0
+  //   }
+  //   function compareDes(a, b) {
+  //     if (a.firstName < b.firstName) {
+  //       return 1;
+  //     }
+  //     if (a.firstName > b.firstName) {
+  //       return -1;
+  //     }
+
+  //     return 0
+  //   }
+  //   if (this.state.sortCol === "") {
+  //     this.setState({ employeeSort: employees })
+  //   } else {
+  //     if (this.state.order) {
+  //       let sorted = list.sort(compareAsc())
+  //       this.setState({ employeeSort: sorted })
+  //     } else {
+  //       let sorted = list.sort(compareDes())
+  //       this.setState({ employeeSort: sorted })
+  //     }
+  //   }
+
+
+  // }
+
+  handleClick = event => {
+    if (this.state.sortCol === event.target.id) {
+      this.setState({ order: !this.state.order })
+    } else {
+      this.setState({ sortCol: event.target.id, order: true });
+    }
+    // this.sortEmployees()
+  }
 
   render() {
     return (
@@ -11,16 +63,16 @@ export default class Table extends Component {
         <thead>
 
           <tr>
-            <th><button>First Name</button></th>
-            <th><button>Last Name</button></th>
-            <th><button>Position</button></th>
-            <th><button>Location</button></th>
+            <th><button onClick={this.handleClick} id="firstName">First Name</button></th>
+            <th><button onClick={this.handleClick} id="lastName">Last Name</button></th>
+            <th><button onClick={this.handleClick} id="position">Position</button></th>
+            <th><button onClick={this.handleClick} id="location">Location</button></th>
           </tr>
         </thead>
         <tbody>
 
           {
-            employees.map(person => (
+            this.state.employeeSort.map(person => (
               <tr key={person.empid}>
                 <td>{person.firstName}</td>
                 <td>{person.lastName}</td>
