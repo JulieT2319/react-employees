391db4cfb3e48fc3139dc3120d61db71589a7aaf
Julie T <joules2319@gmail.com>
2020-04-13 02:24:24 -0600

    made Main the state holding part of the app

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Table from "../components/Table"
 import { SearchContainer } from '../components/SearchContainer'
+import { employees } from '../utils/employees'
 
-export default function Main() {
+export default class Main extends Component {
 
-  return (
-    <div className="center container">
-      <SearchContainer />
-      <Table />
+  state = {
+    employeeSort: employees,
+    sortCol: "",
+    order: true,
+    search: ""
+  }
 
-    </div>
-  );
+  handleClick = event => {
+    if (this.state.sortCol === event.target.id) {
+      this.setState({ order: !this.state.order })
+    } else {
+      this.setState({ sortCol: event.target.id, order: true });
+    }
+    // this.sortEmployees()
+  }
 
+  render() {
+    return (
+      <div className="center container">
+        <SearchContainer />
+
+        <Table
+          employeeSort={this.state.employeeSort}
+          handleClick={this.handleClick}
+        />
+
+      </div>
+    )
+  }
 }
+
